a36e19028c4784bf5232cc40f6aeaea1fa71ba5c
wooniekim <dawun3295@gmail.com>
2022-05-15 18:16:50 +0900

    프런트 단 javascript파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require('./src/routes/home'); // routes의 home폴더 안에 있는
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드, "/"경로로 오면 home으로 이동
 
